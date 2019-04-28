bce0505312d27c4033766d8db4a314507d228ede
Boutinom <maxence.boutinot@protonmail.com>
2019-04-28 18:13:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,29 +7,8 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     var message = msg.content;
-    
-    if (message.substring(0, 5) == '!roll') {
-      //var arg = message.substring(6, message.length-1);
-      //var sep = arg.indexOf("d");
-      //var mult = Number(arg.substring(0, sep));
-      //var type = Number(arg.substring(sep + 1, arg.length));
 
-      //var dices = [];
-      //var sum = 0;
-      //for (i=0; i<mult; i++){
-        //dices[i] = Math.floor((Math.random() * type) + 1);
-        //sum = sum + dices[i];
-      //}
-
-     //var res = dices[0] + "";
-      //if (dices.length > 1) {
-        //for(i=1; i<dices.length; i++){
-          //res = res + " + " + dices[i];
-        //}
-        //res = res + " = " + sum;
-      //}
-
-      msg.reply('good');
+      msg.reply(message);
 });
 
 // THIS  MUST  BE  THIS  WAY
