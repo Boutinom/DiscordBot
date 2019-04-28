5f9021c3ffb92169fe7024895d8ed9e2ff1b0b30
Boutinom <maxence.boutinot@protonmail.com>
2019-04-28 21:51:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,28 +6,36 @@ client.on('ready', () => {
 });
 
 client.on('message', function (user, userID, channelID, message, evt) {
-    
-    if (message.substring(0, 1) == '!') {
-      var par1 = message.indexOf("(");
-      var func = message.substring(1, par1);
-      var arg = message.substring(par1 + 1, message.length-1);
-      var res = "";
-        
-      switch(func) {
-        case "help":
-            res = help();
-        break;
-        case "roll":
-            res = roll(arg);
-        break;
-        default:
-            res = "Fonction inconnue. Utilise !help() pour visualiser la liste des fonctions disponibles."
-} 
-        
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 5) == '!roll') {
+      var arg = message.substring(6, message.length-1);
+      var sep = arg.indexOf("d");
+      var mult = Number(arg.substring(0, sep));
+      var type = Number(arg.substring(sep + 1, arg.length));
+
+      var dices = [];
+      var sum = 0;
+      for (i=0; i<mult; i++){
+        dices[i] = Math.floor((Math.random() * type) + 1);
+        sum = sum + dices[i];
+      }
+
+      var res = dices[0] + "";
+      if (dices.length > 1) {
+        for(i=1; i<dices.length; i++){
+          res = res + " + " + dices[i];
+        }
+        res = res + " = " + sum;
+      }
+
       client.sendMessage({
         to: channelID,
         message: res
       });
+
+            // Just add any case commands if you want to..
+     }
 });
 
 function roll(arg) {
