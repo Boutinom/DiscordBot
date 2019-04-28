0b1bf6af636276463e61700586c0a549c1f9f4bc
Boutinom <maxence.boutinot@protonmail.com>
2019-04-28 22:24:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    message.channel.send("TEST");
+    
     var msg = message.content;
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
