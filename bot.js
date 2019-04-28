53c3775ac5b1a1e19620a8044c836b267fa652f3
Boutinom <maxence.boutinot@protonmail.com>
2019-04-28 22:29:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,33 +6,17 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    message.channel.send("TEST");
     
     var msg = message.content;
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (msg.substring(0, 5) == '!roll') {
-      var arg = msg.substring(6, msg.length-1);
-      var sep = arg.indexOf("d");
-      var mult = Number(arg.substring(0, sep));
-      var type = Number(arg.substring(sep + 1, arg.length));
+    if (msg.substring(0, 1) == '!') {
+      var par1 = msg.indexOf("(");
+      var func = msg.substring(1, par1);
+      var arg = msg.substring(par1 + 2, msg.length-1);
 
-      var dices = [];
-      var sum = 0;
-      for (i=0; i<mult; i++){
-        dices[i] = Math.floor((Math.random() * type) + 1);
-        sum = sum + dices[i];
-      }
 
-      var res = dices[0] + "";
-      if (dices.length > 1) {
-        for(i=1; i<dices.length; i++){
-          res = res + " + " + dices[i];
-        }
-        res = res + " = " + sum;
-      }
-
-      message.channel.send(res);
+      message.channel.send(func + arg);
 
             // Just add any case commands if you want to..
      }
