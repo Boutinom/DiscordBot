6e3ed1be463339a5ca4c8bda0a48cfe71491fa9e
Boutinom <maxence.boutinot@protonmail.com>
2019-04-28 21:25:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const roll = require('./roll');
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -11,10 +10,33 @@ client.on('message', msg => {
     
     if (message.substring(0, 5) == '!roll') {
       var arg = message.substring(6, message.length-1);
-      var res = roll.rollDice(arg);
+      var res = roll(arg);
         
       msg.reply(res);}
 });
+
+function roll(arg) {
+      var sep = arg.indexOf("d");
+      var mult = Number(arg.substring(0, sep));
+      var type = Number(arg.substring(sep + 1, arg.length));
+
+      var dices = [];
+      var sum = 0;
+      for (i=0; i<mult; i++){
+        dices[i] = Math.floor((Math.random() * type) + 1);
+        sum = sum + dices[i];
+      }
+
+      var res = dices[0] + "";
+      if (dices.length > 1) {
+        for(i=1; i<dices.length; i++){
+          res = res + " + " + dices[i];
+        }
+        res = res + " = " + sum;
+      }
+    
+    return res;
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
