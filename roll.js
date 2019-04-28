0c4976a1b7dfd633bde1b50e088b340829f4d827
Boutinom <maxence.boutinot@protonmail.com>
2019-04-28 21:17:11 +0200

    Update roll.js

--- roll.js
+++ roll.js
@@ -1,24 +1,5 @@
 module.exports = {
-   rollDice: function(dice) {
-      var sep = arg.indexOf("d");
-      var mult = Number(arg.substring(0, sep));
-      var type = Number(arg.substring(sep + 1, arg.length));
-
-      var dices = [];
-      var sum = 0;
-      for (i=0; i<mult; i++){
-        dices[i] = Math.floor((Math.random() * type) + 1);
-        sum = sum + dices[i];
-      }
-
-      var res = dices[0] + "";
-      if (dices.length > 1) {
-        for(i=1; i<dices.length; i++){
-          res = res + " + " + dices[i];
-        }
-        res = res + " = " + sum;
-      }
-      
-     return res;
+   rollDice: function() {
+     return "Pong";
    }
 }
