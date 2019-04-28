1c1a3dad5c320dbd30a67f0199a5cc37aa8f605f
Boutinom <maxence.boutinot@protonmail.com>
2019-04-28 21:12:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,24 +11,7 @@ client.on('message', msg => {
     
     if (message.substring(0, 5) == '!roll') {
       var arg = message.substring(6, message.length-1);
-      var sep = arg.indexOf("d");
-      var mult = Number(arg.substring(0, sep));
-      var type = Number(arg.substring(sep + 1, arg.length));
-
-      var dices = [];
-      var sum = 0;
-      for (i=0; i<mult; i++){
-        dices[i] = Math.floor((Math.random() * type) + 1);
-        sum = sum + dices[i];
-      }
-
-      var res = dices[0] + "";
-      if (dices.length > 1) {
-        for(i=1; i<dices.length; i++){
-          res = res + " + " + dices[i];
-        }
-        res = res + " = " + sum;
-      }
+      var res = roll.rollDice(arg);
         
       msg.reply(res);}
 });
